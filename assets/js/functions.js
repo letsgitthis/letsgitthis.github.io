7b956f181988e5e6cb0b27eb6145b4e6b89a369b
John Bentley <jbentley1011@gmail.com>
2020-06-18 22:43:42 -0500

    replaced bootstrap with materialize

---
+++ assets/js/functions.js
@@ -0,0 +1,35 @@
+
+// this runs a specific carousel carousel - test click function
+// $(document).ready(function () {
+//   $(".carousel.carousel-slider").carousel({
+//       fullWidth: true,
+//       indicators: true,
+//   });
+// });
+
+// this runs the index carousel
+$(document).ready(function () {
+  $(".carousel").carousel();
+});
+
+// this runs materialbox, but does not effect youtube videos
+$(document).ready(function () {
+  $(".materialboxed").materialbox();
+});
+
+
+//  this runs parallax
+$(document).ready(function () {
+  $(".parallax").parallax();
+});
+
+
+// this runs the dropdown
+$(document).ready(function () {
+  $('.collapsible').collapsible();
+});
+
+// this runs the mobile side bar
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
